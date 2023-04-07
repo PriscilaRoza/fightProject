48d76c0401b055e916c4f1727877b11dd011fea2
Priscila Roza <priscilarozasilva@gmail.com>
2023-04-07 18:35:10 -0300

    Visualizando monstro via console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 let char = new Knight('Priscila');
+let monster = new BigMonster();
 
 console.log(`Nome do Herói: ${char.name}`);
 console.log(`Vida: ${char.life}`);
 console.log(`Ataque: ${char.attack}`);
+
+console.log(monster.name)
+console.log(`Vida do monstro: ${monster.life}`)
+console.log(`defesa do monstro: ${monster.defense}`);
