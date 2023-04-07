410e8e2c9e5febf2577ba905ac5bf9d09f0940c0
Priscila Roza <priscilarozasilva@gmail.com>
2023-04-07 19:17:29 -0300

    visualizando o nome dos personagens na tela e quantidade de vida

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -58,3 +58,24 @@ class BigMonster extends Character {
     this.maxLife = this.life;
   }
 }
+
+//Cenário
+
+class Stage {
+    constructor(fighter1, fighter2, figter1El, fighter2El) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1El = figter1El;
+        this.fighter2El = fighter2El;
+    }
+    start() {
+         this.update()
+    }
+    update() {
+        // Fighter 1
+        this.fighter1El.querySelector('.name').innerHTML = this.fighter1.name;
+
+        //fighter 2
+         this.fighter2El.querySelector(".name").innerHTML = this.fighter2.name;
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 let char = new Knight('Priscila');
 let monster = new BigMonster();
 
-console.log(`Nome do Herói: ${char.name}`);
-console.log(`Vida: ${char.life}`);
-console.log(`Ataque: ${char.attack}`);
-
-console.log(monster.name)
-console.log(`Vida do monstro: ${monster.life}`)
-console.log(`defesa do monstro: ${monster.defense}`);
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+);
+stage.start()
