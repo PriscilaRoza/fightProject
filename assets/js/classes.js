5165ec523c95aba8534d2b1dbeddb12457d80842
Priscila Roza <priscilarozasilva@gmail.com>
2023-04-07 21:42:49 -0300

    botão de ataque e barra de vida funcionando

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -77,18 +77,40 @@ class Stage {
     }
     update() {
         // Fighter 1
-        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`;
+        this.fighter1El.querySelector(".name").innerHTML = `${
+          this.fighter1.name
+        } - ${this.fighter1.life.toFixed(2)} HP`;
         let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
         this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`
 
         //fighter 2
-        this.fighter2El.querySelector(".name").innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
+        this.fighter2El.querySelector(".name").innerHTML = `${
+          this.fighter2.name
+        } - ${this.fighter2.life.toFixed(2)} HP`;
         let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
         this.fighter2El.querySelector(".bar").style.width = `${f2Pct}%`;
 
     }
 
     doAttack(attacking, attacked) {
-        console.log(`${attacking.name} está atacando ${attacked.name}`)
+        if (attacking.life <= 0 || attacked.life <= 0) {
+            console.log(`Alguém morreu`)
+            return;
+        }
+
+        let attackFactor = (Math.random() * 2).toFixed(2);
+        let defenseFactor = (Math.random() * 2).toFixed(2);
+        console.log(attackFactor);
+        let actualAttack = attacking.attack * attackFactor;
+        let actualDefense = attacked.defense * defenseFactor;
+
+        if (actualAttack > actualDefense) {
+            attacked.life -= actualAttack;
+            console.log(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`)
+        } else {
+            console.log(`${attacked.name} conseguiu se denfender`)
+        }
+
+        this.update();
     }
 }
